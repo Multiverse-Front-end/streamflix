3c81065dea23d95456cc6ae4551f4a8bf65ef2d1
hector <hpulido1021@gmail.com>
2024-05-02 14:45:28 -0400

    Feature: created setup of the API KEY, started styling and home component to retrieve data

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
+    
+      <h1 className='text-white text-4xl font-bold cursor-pointer'>
+        STREAMFLIX
+      </h1>
+      <div>
+       
+          <button className='text-white pr-4'>Account</button>
+
+        <button
+        
+          className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'
+        >
+          Logout
+        </button>
+      </div>
+      <div>
+      
+          <button className='text-white pr-4'>Sign In</button>
+       
+          <button className='bg-red-600 px-6 py-2 rounded cursor-pointer text-white'>
+            Sign Up
+          </button>
+
+      </div>
+    )
+  </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+// import  Navbar from "../components/Navbar";
+import Main from '../components/Main';
 
 const Home = () => {
   return (
-    <div>Home</div>
+
+    <Main />
   )
 }
 
