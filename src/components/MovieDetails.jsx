1ba96a48be768f7cc3b9148820695f3d15456c87
hector <hpulido1021@gmail.com>
2024-05-24 15:30:32 -0400

    Feature: added style to the movie detail page

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const MovieDetails = ({ item, onClose }) => {
   return (
     <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-75 z-50'>
-      <div className='bg-white rounded-lg p-6 max-w-md mx-auto relative'>
-        <button
-          onClick={onClose}
-          className='absolute top-4 right-4 bg-red-600 text-white rounded-full w-8 h-8 flex items-center justify-center text-xl'
-        >
-          &times;
-        </button>
+      <div className='bg-black rounded-lg p-6 max-w-md mx-auto relative'>
+          <p onClick={onClose} className='absolute text-red-500 top-2 right-2'><AiOutlineClose /></p>
         <img
           className='w-full h-auto mb-4'
           src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
           alt={item?.title}
         />
-        <h2 className='text-2xl font-bold mb-2'>{item?.title}</h2>
-        <p className='text-gray-700 mb-4'>{item?.overview}</p>
-        <p className='text-gray-500'>Release Date: {item?.release_date}</p>
+        <h2 className='text-white font-bold mb-2'>{item?.title}</h2>
+        <p className=' text-white mb-4'>{item?.overview}</p>
+        <p className=' text-white'>Release Date: {item?.release_date}</p>
       </div>
     </div>
   );
