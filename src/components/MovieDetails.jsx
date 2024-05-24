1be461a34bd67c1a5ac872b54b8f67c6b8f6af7f
hector <hpulido1021@gmail.com>
2024-05-24 15:52:02 -0400

    Feature: fixed text on the movieDetail component

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -4,15 +4,15 @@ import { AiOutlineClose } from 'react-icons/ai';
 const MovieDetails = ({ item, onClose }) => {
   return (
     <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-75 z-50'>
-      <div className='bg-black rounded-lg p-6 max-w-md mx-auto relative'>
-          <p onClick={onClose} className='absolute text-red-500 top-2 right-2'><AiOutlineClose /></p>
+      <div className='bg-black rounded-lg p-6 max-w-3xl w-full mx-4 relative overflow-hidden'>
+          <p onClick={onClose} className='absolute text-white top-1 right-1'><AiOutlineClose size={24} /></p>
         <img
           className='w-full h-auto mb-4'
           src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
           alt={item?.title}
         />
-        <h2 className='text-white font-bold mb-2'>{item?.title}</h2>
-        <p className=' text-white mb-4'>{item?.overview}</p>
+        <h2 className='text-white font-bold text-2xl mb-2'>{item?.title}</h2>
+        <p className=' text-white mb-4 max-w-auto overflow-y-auto whitespace-normal break-words' >{item?.overview}</p>
         <p className=' text-white'>Release Date: {item?.release_date}</p>
       </div>
     </div>
